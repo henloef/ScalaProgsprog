76ee732023fc621c211208bf4f1601fefd28ac2d
Øystein Hauan <oysteibh@stud.ntnu.no>
2015-10-25 11:44:23 +0100

    Bank

--- part1-exercise/src/main/scala/Account.scala
+++ part1-exercise/src/main/scala/Account.scala
@@ -1,10 +1,27 @@
+import exceptions.{NoSufficientFundsException, IllegalAmountException}
 
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
+  var balance: Double = initialBalance
   
-  def withdraw(amount: Double): Unit = ??? //Implement
+  def withdraw(amount: Double): Unit = {
+    if(amount > getBalanceAmount){
+      throw new NoSufficientFundsException();
+    }
+    else if(amount < 0){
+      throw new IllegalAmountException()
+    }
+    balance = balance - amount
+  } //Implement
   
-  def deposit(amount: Double): Unit = ??? //Implement
+  def deposit(amount: Double): Unit = {
+    if(amount <= 0){
+      throw new IllegalAmountException("Bullshit")
+    }
+    balance = balance + amount
+  } //Implement
   
-  def getBalanceAmount: Double = ??? //Implement
+  def getBalanceAmount: Double = {
+    return balance;
+  }//Implement
 
 }
--- part1-exercise/src/main/scala/Bank.scala
+++ part1-exercise/src/main/scala/Bank.scala
@@ -1,17 +1,21 @@
-import exceptions.IllegalAmountException
+import exceptions.{NoSufficientFundsException, IllegalAmountException}
 
 object Bank {
   
   private var idCounter: Int = 0
   
   def transaction(from: Account, to: Account, amount: Double): Unit = {
-    if(from.getBalanceAmount > amount){
-      throw new IllegalAmountException("Insufficient funds")
+    if(from.getBalanceAmount < amount){
+      throw new NoSufficientFundsException("Insufficient funds")
+    }
+    else if(amount <= 0){
+      throw new IllegalAmountException("blbk")
     }
     else {
       from.withdraw(amount)
       to.deposit(amount)
     }
+
 
   } //Implement
   
